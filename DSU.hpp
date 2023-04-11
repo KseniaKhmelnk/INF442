35296af72168d594f5a885bc26c9cde87e814f09
czartur <arturalves.ca@gmail.com>
2023-04-11 12:12:30 +0200

    add

---
+++ DSU.hpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <vector>
+
+class DSU {
+  private: 
+    int* par;
+    int* rank;
+
+  public:
+    int size;
+    
+    DSU(int n) : size{n}{
+        par = new int[n];
+        rank = new int[n]();
+        for(int i=0; i<n; ++i) par[i] = i;
+    }
+    int find(int a){
+        if(par[a] == a) return a;
+        return par[a] = find(par[a]);
+    }
+
+    void join(int a, int b){
+        a=find(a), b=find(b);
+        if(a != b) size--;
+        if(rank[a] < rank[b]) std::swap(a,b);
+        par[a] = b;
+        rank[b] += rank[a];
+    }
+
+    void set_parent(int a, int label){
+      par[a] = label;
+    }
+}; 
--- dbscan.cpp
+++ dbscan.cpp
@@ -3,6 +3,7 @@
 #include <queue>
 #include <cmath>
 #include "dbscan.hpp"
+#include "DSU.hpp"
 
 using namespace std;
 
@@ -76,22 +77,54 @@ int expandCluster(vector<Point> &List, Point &point, int minPts, int clusterID, 
     }
 }
 
+int expandCluster(vector<Point> &List, Point &point, int minPts, int clusterID, double eps, DSU &UF){
+    
+    UF.set_parent(point.number, clusterID);
+    vector<Point> cluster = calculateCluster(List, point, eps);
+    for(auto& neighbor : cluster){
+        if(List[neighbor.number].label >= 0){ // classified
+            UF.join(List[neighbor.number].label, clusterID);
+        }
+        List[neighbor.number].label = UF.find(clusterID);
+        // printf("%d %d\n", neighbor.number, UF.find(clusterID));
+    }
+
+    return SUCCESS;
+}
 
 int dbscan(vector<Point> &List, int minPts, double eps)
 {
-    int clusterID = 0;
-    for(auto p: List)
-    {
-        if (p.label == UNCLASSIFIED)
-        {
-            if (expandCluster(List, p, minPts, clusterID, eps) != FAILURE)
-            {
-                clusterID += 1;
-            }
+    // int clusterID = 0;
+    // for(auto point: List)
+    // {
+    //     if (point.label == UNCLASSIFIED)
+    //     {
+    //         if (expandCluster(List, point, minPts, clusterID, eps) != FAILURE)
+    //         {
+    //             clusterID += 1;
+    //         }
+    //     }
+    //    printf("%d %d %d\n", point.label, point.x, point.y);
+    // }
+    // return clusterID;
+
+    DSU UF((int) List.size());
+    vector<int> core_points_id;
+    for(auto &point : List){
+        if(calculateCluster(List, point, eps).size() >= minPts){
+            core_points_id.push_back(point.number);
         }
-       printf("%d %d %d\n", p.label, p.x, p.y);
     }
-    return clusterID; //how many clusters exist
+
+    for(auto id : core_points_id){
+        auto& point = List[id];
+        expandCluster(List, point, minPts, point.number, eps, UF);
+    }
+
+    for(auto &point : List){
+        printf("%d %d %d\n", point.label, point.x, point.y);
+    }
+    return UF.size-(List.size()-core_points_id.size()); //how many clusters exist
 }
 
 
