7f68e3dffd5d0e3326da1e7161f28b7abfb10135
czartur <arturalves.ca@gmail.com>
2023-04-11 16:46:37 +0200

    corrected class

--- dbscan.cpp
+++ dbscan.cpp
@@ -11,21 +11,21 @@ double Point::calculateDistance(const Point& pointTarget)
     return sqrt(pow(this->x - pointTarget.x, 2) + pow(this->y - pointTarget.y, 2));
 }
 
-vector<Point> DBSCAN::calculateCluster(vector<Point> &List, Point &point, double eps)
-{
-    int index = 0;
-    //initialize cluster_eps
-    vector<Point> cluster_eps;
-    for(auto v: List)
-    {
-        if (point.calculateDistance(v) <= eps)
-        {
-            cluster_eps.push_back(v);
-        }
-       // printf("%f %d %d\n",calculateDistance(ppoint, v), ppoint.ID, v.ID );
-    }
-    return cluster_eps;
-}
+// vector<int> DBSCAN::calculateCluster(vector<Point> &data, Point &center_point)
+// {
+//     int index = 0;
+//     //initialize cluster_eps
+//     vector<int> cluster_eps;
+//     for(auto point : data)
+//     {
+//         if (center_point.calculateDistance(point) <= eps)
+//         {
+//             cluster_eps.push_back(point.ID);
+//         }
+//        // printf("%f %d %d\n",calculateDistance(ppoint, v), ppoint.ID, v.ID );
+//     }
+//     return cluster_eps;
+// }
 
 //Old version complexity O(n ^ 2)
 // int expandCluster(vector<Point> &List, Point &point, int minPts, int clusterID, double eps)
@@ -77,23 +77,67 @@ vector<Point> DBSCAN::calculateCluster(vector<Point> &List, Point &point, double
 //     }
 // }
 
-int DBSCAN::expandCluster(vector<Point> &List, Point &point, int minPts, int clusterID, double eps, DSU &UF){
-    
-    UF.set_parent(point.ID, clusterID);
-    vector<Point> cluster = calculateCluster(List, point, eps);
-    for(auto& neighbor : cluster){
-        if(List[neighbor.ID].label >= 0){ // classified
-            UF.join(List[neighbor.ID].label, clusterID);
+int DBSCAN::expandCluster(vector<Point> &data, Point &center_point){
+    vector<int> cluster_eps;
+    for(auto point : data)
+    {
+        if (center_point.calculateDistance(point) <= eps)
+        {
+            cluster_eps.push_back(point.ID);
         }
-        List[neighbor.ID].label = UF.find(clusterID);
-        // printf("%d %d\n", neighbor.ID, UF.find(clusterID));
+    }
+
+    if(cluster_eps.size() < min_pts) return FAILURE;
+
+    for(auto& neighbor_ID : cluster_eps){
+        if(data[neighbor_ID].label >= 0){ // classified
+            disjoint_set->join(data[neighbor_ID].label, center_point.ID);
+        }
+        data[neighbor_ID].label = disjoint_set->find(center_point.ID);
     }
 
     return SUCCESS;
 }
 
-int DBSCAN::fit(vector<Point> &List, int minPts, double eps)
+int DBSCAN::fit(vector<Point> &data)
 {
+    disjoint_set = new DSU((int) data.size());;
+    
+    int noises = 0;
+    for(auto &point : data){
+        if(calculateCluster(data, point).size() >= min_pts){
+            ++noises;
+            expandCluster(data, point);
+        }
+    }
+
+    for(auto &point : data){
+        printf("%d %d %d\n", point.label, point.x, point.y);
+    }
+
+    return disjoint_set->size-(data.size()-noises); //how many clusters exist
+}
+
+int main()
+{
+    vector<Point> List;
+    double eps = 3;
+    int m_minPts = 2, n;
+    int i = 0;
+    cin >> n;
+    DBSCAN clustering(eps, m_minPts);
+    while(n--)
+    {
+        int x, y;
+        cin >> x >> y;
+        List.push_back(Point(x, y, i));
+        i++; 
+    }
+    printf("%d\n", clustering.fit(List));
+}
+
+// FIT
+// VERSION 1
     // int clusterID = 0;
     // for(auto point: List)
     // {
@@ -108,40 +152,15 @@ int DBSCAN::fit(vector<Point> &List, int minPts, double eps)
     // }
     // return clusterID;
 
-    DSU UF((int) List.size());
-    vector<int> core_points_id;
-    for(auto &point : List){
-        if(calculateCluster(List, point, eps).size() >= minPts){
-            core_points_id.push_back(point.ID);
-        }
-    }
+    // VERSION 2
+    // vector<int> core_points_id;
+    // for(auto &point : List){
+    //     if(calculateCluster(List, point).size() >= min_pts){
+    //         core_points_id.push_back(point.ID);
+    //     }
+    // }
 
-    for(auto id : core_points_id){
-        auto& point = List[id];
-        expandCluster(List, point, minPts, point.ID, eps, UF);
-    }
-
-    for(auto &point : List){
-        printf("%d %d %d\n", point.label, point.x, point.y);
-    }
-    return UF.size-(List.size()-core_points_id.size()); //how many clusters exist
-}
-
-
-int main()
-{
-    vector<Point> List;
-    DBSCAN clustering;
-    double eps = 3;
-    int m_minPts = 2, n;
-    int i = 0;
-    cin >> n;
-    while(n--)
-    {
-        int x, y;
-        cin >> x >> y;
-        List.push_back(Point(x, y, i));
-        i++; 
-    }
-    printf("%d\n", clustering.fit(List, m_minPts, eps));
-}
+    // for(auto id : core_points_id){
+    //     auto& point = List[id];
+    //     expandCluster(List, point);
+    // }
