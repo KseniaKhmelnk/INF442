520df62b4bc40a1f484a8ea4e68e739a30fdf1b4
czartur <arturalves.ca@gmail.com>
2023-05-01 01:01:46 +0200

    fix DSU

--- dbscan.cpp
+++ dbscan.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include "dbscan.hpp"
 
@@ -12,22 +11,24 @@ std::vector<int> DBSCAN::getNeighbors(int point_id, int total_pts, double** dist
     return neighbors;
 }
 
-
 void DBSCAN::fit_dsu(double** distance_matrix, int total_pts){
     DSU disjoint_set(total_pts);
 
+    std::vector<bool> classified;
     for(int point = 0; point < total_pts; ++point){
         std::vector<int> neighbors = getNeighbors(point, total_pts, distance_matrix);
-        if(neighbors.size() < min_pts - 1) continue;
+        
+        if(neighbors.size() < min_pts - 1) continue; // noise
+        labels[point] = CORE;
+
         for(auto neighbor : neighbors){
+            if(labels[neighbor] == BORDER) continue; // classified as border
             disjoint_set.join(neighbor, point);
-            labels[neighbor] = disjoint_set.find(point);
+            if(labels[neighbor] == NOISE) labels[neighbor] = BORDER;
         }
-        labels[point] = disjoint_set.find(point);
     }
 
     std::vector<int> reindex(total_pts, -1); // -1 noise or not reindexed
-    int n_cluster = -1;
     
     for(int point = 0; point < total_pts; ++point){
         if(~labels[point]){ // not noise
@@ -35,13 +36,11 @@ void DBSCAN::fit_dsu(double** distance_matrix, int total_pts){
             if(reindex[id] == -1) reindex[id] = ++n_cluster;
             labels[point] = reindex[id];
         }
-    }
-    
+    } 
 }
 
 void DBSCAN::fit_bfs(double** distance_matrix, int total_pts){
     std::vector<bool> visited(total_pts);
-    int n_cluster = -1;
 
     for(int point = 0; point < total_pts; ++point){
         
@@ -73,44 +72,11 @@ void DBSCAN::fit_bfs(double** distance_matrix, int total_pts){
 void DBSCAN::fit(double** distance_matrix, int total_pts, std::string algo){
     // cleaning labels 
     labels.clear();
-    labels.resize(total_pts, -1); // -1 if noise
+    labels.resize(total_pts, NOISE); // -1 if noise
+    n_cluster = 0;
 
     if (algo != "BFS" && algo != "DSU") 
         throw std::invalid_argument("\'" + algo + "\'" + " is not a valid method");
-    
+
     algo == "BFS" ? fit_bfs(distance_matrix, total_pts) : fit_dsu(distance_matrix, total_pts);
 } 
-
-// testing
-using namespace std;
-
-int main() 
-{
-    vector<Point> List;
-    double eps = 3;
-    int m_minPts = 2, n;
-    int i = 0;
-    cin >> n;
-    while(n--)
-    {
-        int x, y;
-        cin >> x >> y;
-        List.push_back(Point(x, y, i));
-        i++; 
-    }
-    
-    int total_pts = List.size();
-    double** distance_matrix = new double*[total_pts];
-    for(int i = 0; i < total_pts; ++i){
-        distance_matrix[i] = new double[total_pts];
-        for(int j = 0; j < total_pts; ++j){
-            distance_matrix[i][j] = List[i].calculateDistance(List[j]);
-        }
-    }
-
-    DBSCAN clustering(eps, m_minPts);
-    clustering.fit(distance_matrix, total_pts, "DS");
-    
-    for(auto id : clustering.labels) cout << id << " ";
-    cout << endl;
-}
--- dbscan.hpp
+++ dbscan.hpp
@@ -1,40 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include "DSU.hpp"
 
-#define UNCLASSIFIED -1
-
-struct Point{  
-    int x; // first coordinate
-    int y; // second coordinate 
-    int label; //label
-    int ID; //id of every point
-    
-    Point(int n_x, int n_y, int n_ID);
-    double calculateDistance(const Point& pointTarget);
-};
-
-Point::Point(int n_x, int n_y, int n_ID)
-{
-    x = n_x;
-    y = n_y;
-    label = UNCLASSIFIED;
-    ID = n_ID;
-}
-
-double Point::calculateDistance(const Point& pointTarget)
-{
-    return sqrt(pow(this->x - pointTarget.x, 2) + pow(this->y - pointTarget.y, 2));
-}
+const int NOISE = -1;
+const int BORDER = 0;
+const int CORE = 1;
 
 class DBSCAN
 {
   private:
     double eps = 0;
-    int min_pts = 0;
-  
+    int min_pts = 0; //y compris le centre du cluster
+    
   public:
+    int n_cluster = 0;
     std::vector<int> labels;
   
   private:
@@ -42,8 +21,7 @@ class DBSCAN
     void fit_dsu(double **distance_matrix, int total_pts);
 
   public:
-    DBSCAN(double n_eps, double n_min_pts):eps{n_eps}, min_pts{n_min_pts}{}
+    DBSCAN(double n_eps, int n_min_pts):eps{n_eps}, min_pts{n_min_pts}{}
     std::vector<int> getNeighbors(int point, int total_pts, double** distance_matrix);
     void fit(double **distance_matrix, int total_pts, std::string algo="BFS");
-    void fit(std::vector<Point> &data);
 };
